df94b5232ff9d568e47112cc7ebec1fdd6ea7353
Mitch Kennedy <mitchk@runbox.com>
2024-10-13 09:40:33 +1100

    work

--- usefull/int_info/src/main.zig
+++ usefull/int_info/src/main.zig
@@ -1,43 +1,58 @@
 const std = @import("std");
 
 const PrintBuf = std.BoundedArray(u8, 64);
-
+const BaseInfo = struct {
+    base: u8,
+    name: []const u8,
+};
+const Base = [_]BaseInfo{
+    .{ .base = 8, .name = "octal" },
+    .{ .base = 16, .name = "hex" },
+    .{ .base = 10, .name = "decimal" },
+};
 pub const PrintedInfo = struct {
-    data_type: PrintBuf = .{},
-    def: PrintBuf = .{},
-    output: PrintBuf = .{},
+    hex: PrintBuf = .{},
+    oct: PrintBuf = .{},
+    dec: PrintBuf = .{},
+    leb: PrintBuf = .{},
+    bin: PrintBuf = .{},
     pub fn format(self: PrintedInfo, _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
-        try std.fmt.format(writer, "{s}\n{s}\t{s}\n", .{
-            self.data_type.slice(),
-            self.def.slice(),
-            self.output.slice(),
-        });
+        try std.fmt.format(writer, "\n{s:>32}\n", .{self.hex.slice()});
+        try std.fmt.format(writer, "{s:>32}\n", .{self.oct.slice()});
+        try std.fmt.format(writer, "{s:>32}\n", .{self.dec.slice()});
+        try std.fmt.format(writer, "{s:>32}\n", .{self.leb.slice()});
+        try std.fmt.format(writer, "{s:>32}\n", .{self.bin.slice()});
     }
 };
 
 pub fn int_print(
     comptime T: type,
     val: T,
-    list: *std.ArrayList(PrintedInfo),
-) !void {
-    const BaseInfo = struct {
-        base: u8,
-        name: []const u8,
-    };
-    const Base = [_]BaseInfo{
-        .{ .base = 8, .name = "octal" },
-        .{ .base = 16, .name = "hex" },
-        .{ .base = 10, .name = "decimal" },
-    };
-
+) !PrintedInfo {
+    var ret = PrintedInfo{};
     const Tinfo = @typeInfo(T).int;
-    for (Base) |b| {
-        var res = PrintedInfo{};
-        try std.fmt.formatInt(val, b.base, .lower, .{}, res.output.writer());
-        try std.fmt.format(res.def.writer(), "{s}", .{b.name});
-        try std.fmt.format(res.data_type.writer(), "{}", .{T});
-        try list.append(res);
-    }
+
+    try std.fmt.formatInt(val, 16, .upper, .{
+        .width = @bitSizeOf(T) / 8,
+        .alignment = .right,
+        .fill = '0',
+    }, ret.hex.writer());
+    try std.fmt.formatInt(val, 8, .upper, .{
+        .width = @bitSizeOf(T) / 4,
+        .alignment = .right,
+        .fill = '0',
+    }, ret.oct.writer());
+    try std.fmt.formatInt(val, 10, .upper, .{
+        .width = @bitSizeOf(T),
+        .alignment = .right,
+        .fill = '0',
+    }, ret.dec.writer());
+    try std.fmt.formatInt(val, 2, .upper, .{
+        .width = @bitSizeOf(T),
+        .alignment = .right,
+        .fill = '0',
+    }, ret.bin.writer());
+
     {
         var leb_buffer = std.mem.zeroes([16]u8);
         var fbs = std.io.fixedBufferStream(leb_buffer[0..]);
@@ -45,25 +60,9 @@ pub fn int_print(
             .unsigned => try std.leb.writeUleb128(fbs.writer(), val),
             .signed => try std.leb.writeIleb128(fbs.writer(), val),
         }
-        var res = PrintedInfo{};
-        try std.fmt.format(res.output.writer(), "{}", .{std.fmt.fmtSliceHexUpper(leb_buffer[0..fbs.pos])});
-        try std.fmt.format(res.def.writer(), "LEB128", .{});
-        try std.fmt.format(res.data_type.writer(), "{}", .{T});
+        try std.fmt.format(ret.leb.writer(), "{}", .{std.fmt.fmtSliceHexUpper(leb_buffer[0..fbs.pos])});
     }
-}
-
-const types = [_]type{ u8, u16, u32, u64, i8, i16, i32, i64 };
-
-pub fn print_info(
-    data: []const u8,
-    list: *std.ArrayList(PrintedInfo),
-) !void {
-    inline for (types[0..]) |T| {
-        const val: ?T = std.fmt.parseInt(T, data, 0) catch null;
-        if (val) |v| {
-            try int_print(T, v, list);
-        }
-    }
+    return ret;
 }
 
 pub fn main() !void {
@@ -72,15 +71,25 @@ pub fn main() !void {
     const alloc = gpa.allocator();
     var list = std.ArrayList(PrintedInfo).init(alloc);
     defer list.deinit();
-    const input = "1234";
+    //    const input = "1234";
     var stdout = std.io.getStdOut();
     var bufw = std.io.bufferedWriter(stdout.writer());
     defer _ = bufw.flush() catch {};
 
     const writer = bufw.writer();
-    try print_info(input, &list);
-    try std.fmt.format(writer, "input:{s}\n", .{input});
-    for (list.items) |l| {
-        try std.fmt.format(writer, "{}", .{l});
-    }
+    const result = try int_print(u32, 1234);
+    try std.fmt.format(writer, "input:{}\n", .{result});
 }
+const expected_output =
+    \\ Input: "1234"
+    \\ ├── Hex
+    \\ │   ├── 0x04D2
+    \\ ├── Oct
+    \\ │   ├── 0o2322
+    \\ ├── Dec
+    \\ │   ├── 1234
+    \\ ├── Leb124_signed
+    \\ │   ├── 0o2322
+    \\ ├── Leb124_unsigned
+    \\ │   ├── 0o2322
+;
